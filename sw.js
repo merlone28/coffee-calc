9d98bf41ddb67c4e47e395f8fbcfdac371ad1706
merlone28 <mirko.ridolfi28@gmail.com>
2026-07-13 15:49:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coffee-calc-v1';
+const CACHE = 'coffee-calc-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', e => {
