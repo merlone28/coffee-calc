b03cf23a5890266a526c7b390f696b92905cfc1c
merlone28 <mirko.ridolfi28@gmail.com>
2026-07-20 17:49:32 +0200

    Fix icona maskable e apple-touch-icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'coffee-calc-v11';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
+const CACHE = 'coffee-calc-v12';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
